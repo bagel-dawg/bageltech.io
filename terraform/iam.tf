ddb890070e13a6797c4ebf7ae5c6de65aab06ce9
Bagel <ryan.knauer@swimlane.com>
2022-10-02 01:37:22 -0400

    add terraform job

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -8,6 +8,10 @@ resource "aws_iam_openid_connect_provider" "github_oidc_provider" {
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
+##
+# CI / CD Role
+##
+
 resource "aws_iam_role" "cicd" {
   name = "bageltech-io-cicd"
 
@@ -60,4 +64,37 @@ EOF
 resource "aws_iam_role_policy_attachment" "cicd" {
   role       = aws_iam_role.cicd.name 
   policy_arn = aws_iam_policy.cicd.arn
+}
+
+##
+# Terraform Role
+##
+
+resource "aws_iam_role" "terraform" {
+  name = "bageltech-io-terraform"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Effect = "Allow"
+      Principal = {
+        "Federated" : aws_iam_openid_connect_provider.github_oidc_provider.arn
+      }
+      Condition = {
+        "ForAnyValue:StringLike" = {
+          "token.actions.githubusercontent.com:sub" : [
+            "repo:bagel-dawg/bageltech.io:ref:refs/tags/*",
+            "repo:bagel-dawg/bageltech.io:ref:refs/heads/main",
+            "repo:bagel-dawg/bageltech.io:ref:refs/heads/release*"
+          ]
+        }
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "terraform" {
+  role       = aws_iam_role.terraform.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
